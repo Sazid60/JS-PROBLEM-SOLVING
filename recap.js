504f919d2d5861dbd31e53a350c6910d589a4592
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2026-03-18 07:02:45 +0600

    While and For loop and included with break and continue and some trickey question solved

--- recap.js
+++ recap.js
@@ -1,4 +1,4 @@
-let a = [1,2,3,4,5,6,7]
+let a = [1, 2, 3, 4, 5, 6, 7]
 
 a.push(8)
 console.log(`push result: ${a}`)
@@ -17,3 +17,76 @@ console.log(`slice result: ${a.slice(2, 4)}`)
 
 a.splice(2, 2, 'a', 'b')
 console.log(`splice result: ${a}`)
+
+
+a.splice(2, 2)
+console.log(`splice result: ${a}`)
+
+//  do while loop playground 
+let b = 1;
+
+do {
+  console.log(" do while Number:", b);
+  b++; 
+} while (b <= 5);
+
+//  break statement with for loop playground
+
+for(let i = 1; i <= 10; i++) {
+  if(i === 5) {
+    break; // Exit the loop when i is 5
+  }
+  console.log(" for loop Number:", i);
+}
+
+
+//  break with wile loop playground
+
+while (b <= 10) {
+  if (b === 8) {
+    break; // Exit the loop when b is 8
+  }
+
+  console.log("while loop Number:", b);
+  b++; // increment b
+}
+
+
+
+//  do while with break statement 
+do {
+  if (b === 12) {
+    break; // Exit the loop when b is 12
+  }
+
+  console.log("do while loop Number:", b);
+  b++;
+
+} while (b <= 20);
+
+// do while with continue statement
+
+do {
+  if (b % 2 === 0) {
+    b++; // Increment b before continue to avoid infinite loop
+    continue; // Skip even numbers
+  }
+  console.log("do while loop Number inside continue statement:", b);
+  b++;
+} while (b <= 20);
+
+// for loop with continue statement
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    continue; // Skip even numbers
+  }
+  console.log("for loop Number inside continue statement:", i);
+}
+
+
+// | Feature         | `for` loop                 | `do...while` loop            |
+// | --------------- | -------------------------- | ---------------------------- |
+// | Condition check | Before running             | After running                |
+// | Runs minimum    | 0 times possible           | Always at least 1 time       |
+// | Best use        | Known number of iterations | When action must happen once |
---
+++ trickyquestions.js
@@ -0,0 +1,42 @@
+// let i = 0;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 3);
+
+
+// for (let i = 1; i <= 5; i++) {
+//   if (i === 3) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let i = 5;
+
+// while (i > 0) {
+//   console.log(i);
+// }
+
+
+// for (let i = 0; i < 5; i++) {
+//   if (i === 2) break;
+//   console.log(i);
+// }
+
+// let i = 3;
+
+// do {
+//   console.log(i);
+//   i--;
+// } while (i > 0);
+
+// let i = 0;
+
+// while (i < 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// console.log(i);
